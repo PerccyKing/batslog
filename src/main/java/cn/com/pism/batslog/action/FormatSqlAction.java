271b7aa85acdee825f43df99e71d76803718ef8a
PerccyKing <perccyking@gmail.com>
2020-10-18 20:28:20 +0800

    feat(init): init
    初始化项目

---
+++ src/main/java/cn/com/pism/batslog/action/FormatSqlAction.java
@@ -0,0 +1,29 @@
+package cn.com.pism.batslog.action;
+
+import com.intellij.openapi.actionSystem.AnAction;
+import com.intellij.openapi.actionSystem.AnActionEvent;
+import com.intellij.openapi.actionSystem.CommonDataKeys;
+import com.intellij.openapi.editor.Editor;
+import com.intellij.openapi.editor.SelectionModel;
+import lombok.extern.log4j.Log4j;
+
+/**
+ * @author PerccyKing
+ * @date 2020/10/18 下午 05:30
+ * @version 0.0.1
+ * @since 0.0.1
+ */
+@Log4j
+public class FormatSqlAction extends AnAction {
+
+    @Override
+    public void actionPerformed(AnActionEvent e) {
+        log.info("hello");
+        //获取编辑器对象
+        Editor editor = e.getRequiredData(CommonDataKeys.EDITOR);
+        SelectionModel selectionModel = editor.getSelectionModel();
+        String selectedText = selectionModel.getSelectedText();
+        System.out.println(selectedText);
+
+    }
+}
---
+++ src/main/java/cn/com/pism/batslog/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * @author PerccyKing
+ * @date 2020/10/17 下午 11:25
+ * @version 0.0.1
+ * @since 0.0.1
+ */
+package cn.com.pism.batslog;
