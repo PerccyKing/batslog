cd5b5c1c7fe2d0478eeb28e18898ae092858cb07
PerccyKing <perccyking@gmail.com>
2020-12-08 00:59:47 +0800

    添加部分未通过测试的代码

---
+++ src/main/java/cn/com/pism/batslog/ui/FormatWindow.java
@@ -0,0 +1,19 @@
+package cn.com.pism.batslog.ui;
+
+import lombok.Data;
+
+import javax.swing.*;
+
+/**
+ * @author PerccyKing
+ * @version 0.0.1
+ * @date 2020/12/07 下午 10:29
+ * @since 0.0.1
+ */
+@Data
+public class FormatWindow {
+    private JButton button1;
+    private JButton button2;
+    private JPanel root;
+    private JTextArea textArea1;
+}
