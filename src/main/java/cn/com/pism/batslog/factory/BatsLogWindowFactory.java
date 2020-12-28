73f1b0266cf6a6e858cbe9f771fbd10ff7eda0f8
wangyihuai <wyihuai@cnksi.com>
2020-12-28 14:51:03 +0800

    删除部分无用代码

--- src/main/java/cn/com/pism/batslog/factory/BatsLogWindowFactory.java
+++ src/main/java/cn/com/pism/batslog/factory/BatsLogWindowFactory.java
@@ -2,8 +2,6 @@ package cn.com.pism.batslog.factory;
 
 import cn.com.pism.batslog.BatsLogBundle;
 import cn.com.pism.batslog.ui.FormatConsole;
-import cn.com.pism.batslog.ui.FormatConsoleNew;
-import cn.com.pism.batslog.ui.FormatWindow;
 import cn.com.pism.batslog.ui.SettingForm;
 import cn.com.pism.batslog.util.BatsLogUtil;
 import com.intellij.openapi.project.Project;
@@ -26,7 +24,7 @@ public class BatsLogWindowFactory implements ToolWindowFactory {
     @Override
     public void createToolWindowContent(@NotNull Project project, @NotNull ToolWindow toolWindow) {
         BatsLogUtil.TOOL_WINDOW = (ToolWindowEx) toolWindow;
-        FormatConsoleNew formatConsole = new FormatConsoleNew(project);
+        FormatConsole formatConsole = new FormatConsole(project);
         SettingForm settingForm = new SettingForm(project);
         ContentFactory contentFactory = ContentFactory.SERVICE.getInstance();
         Content formatConsoleContent = contentFactory.createContent(formatConsole.getRoot(), BatsLogBundle.message("console"), false);
