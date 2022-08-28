402fba3bf87df1bf3e2b8dcf768cd1ea74cf8378
PerccyKing <perccyking@gmail.com>
2022-08-28 23:50:43 +0800

    添加对clickhouse的支持，适配2019.2

--- src/main/java/cn/com/pism/batslog/action/CopyAction.java
+++ src/main/java/cn/com/pism/batslog/action/CopyAction.java
@@ -3,7 +3,7 @@ package cn.com.pism.batslog.action;
 import cn.com.pism.batslog.util.BatsLogUtil;
 import com.intellij.openapi.actionSystem.AnAction;
 import com.intellij.openapi.actionSystem.AnActionEvent;
-import com.intellij.openapi.util.NlsActions;
+import org.jetbrains.annotations.Nls;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
@@ -19,7 +19,17 @@ import static cn.com.pism.batslog.util.BatsLogUtil.copyToClipboard;
  */
 public class CopyAction extends AnAction {
 
-    public CopyAction(@Nullable @NlsActions.ActionText String text, @Nullable @NlsActions.ActionDescription String description, @Nullable Icon icon) {
+
+    /**
+     * Constructs a new action with the specified text, description and icon.
+     *
+     * @param text        Serves as a tooltip when the presentation is a button and the name of the
+     *                    menu item when the presentation is a menu item
+     * @param description Describes current action, this description will appear on
+     *                    the status bar when presentation has focus
+     * @param icon        Action's icon
+     */
+    public CopyAction(@Nls(capitalization = Nls.Capitalization.Title) @Nullable String text, @Nls(capitalization = Nls.Capitalization.Sentence) @Nullable String description, @Nullable Icon icon) {
         super(text, description, icon);
     }
 
