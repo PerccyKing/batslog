37fda762e3c9d889f1b394725dda989fb3dc4e6a
PerccyKing <perccyking@gmail.com>
2023-04-02 17:55:38 +0800

    feat 添加新的格式化窗口布局

---
+++ src/main/java/cn/com/pism/batslog/ui/FormatWindow2.java
@@ -0,0 +1,15 @@
+package cn.com.pism.batslog.ui;
+
+import javax.swing.*;
+
+/**
+ * @author PerccyKing
+ * @since 2023/4/2 17:51
+ */
+public class FormatWindow2 {
+    private JPanel panel1;
+    private JTextArea sql;
+    private JTextArea params;
+    private JTextArea res;
+    private JPanel toolbar;
+}
